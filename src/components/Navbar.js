910f7bd679c677725ea8abeddf138c1855e4ba0b
Banstein <islambahnas1@gmail.com>
2022-04-02 17:04:26 +0200

    Update files, request changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './styles/Navbar.css';
 import { Link } from 'react-router-dom';
+import { MdAccountCircle } from 'react-icons/md';
 
 function Navbar() {
   return (
     <nav>
-      <h1>Bookstore CMS</h1>
-      <div className="links">
-        <Link to="/">BOOKS</Link>
-        <Link to="/Categories">CATEGORIES</Link>
+      <div className="nav-wrap">
+        <h1>Bookstore CMS</h1>
+        <div className="links">
+          <Link to="/">BOOKS</Link>
+          <Link to="/Categories">CATEGORIES</Link>
+        </div>
       </div>
+      <MdAccountCircle className="profile" />
     </nav>
   );
 }
